b22d69a81fe9cc33ef66445fadd3d4ce59535758
luobaotai <luobaotai@gmail.com>
2023-12-05 17:51:42 +0800

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='relative'>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
---
+++ utils/index.ts
@@ -0,0 +1,38 @@
+import { FilterProps } from "@/types/index";
+
+export async function fetchCars(filters : FilterProps){
+
+    const { manufacturer, year, fuel, limit, model } = filters;
+    const headers = {
+        'X-RapidAPI-Key': 'b205bf9251mshad83091e67e21fap158318jsnb2bc69a1ed19',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {headers: headers});
+
+    const result = await response.json();
+    console.log(result)
+    return result
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
+
+export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(type, value);
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+    return newPathname;
+}
